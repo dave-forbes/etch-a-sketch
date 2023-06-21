7ace429feb728e626d9198ceca4cdee3d3a457ae
David Forbes <davidforbesi@hotmail.com>
2023-06-21 15:08:24 +0100

    add in GUI and styling

--- scripts.js
+++ scripts.js
@@ -1,11 +1,27 @@
 const gridContainer = document.querySelector('#grid-container');
-const button = document.querySelector('button');
+const slider = document.querySelector('#slider');
+const color = document.querySelector('#color');
+const eraser = document.querySelector('#eraser');
 
-button.addEventListener('click', () => {
+
+
+colorValue = 'black';
+
+color.addEventListener('change', () => colorValue = color.value);
+
+
+window.onload = () => {
+  gridContainer.style.cssText = `grid-template-columns: repeat(50, 1fr); grid-template-rows: repeat(50, 1fr);`;
+  const divs = Array.from({ length: 2500 }, () => document.createElement('div'));
+  gridContainer.append(...divs);
+  divs.forEach((item) => item.addEventListener('mouseover', () => item.style.backgroundColor = colorValue));
+}
+
+slider.addEventListener('change', () => {
 
   gridContainer.innerHTML = '';
 
-  const rowSize = prompt('What size would you like your grid?');
+  const rowSize = slider.value;
 
   gridContainer.style.cssText = `grid-template-columns: repeat(${rowSize}, 1fr); grid-template-rows: repeat(${rowSize}, 1fr);`;
 
@@ -15,6 +31,13 @@ button.addEventListener('click', () => {
 
   gridContainer.append(...divs);
 
-  divs.forEach((item) => item.addEventListener('mouseover', () => item.style.backgroundColor = 'black'));
+  divs.forEach((item) => item.addEventListener('mouseover', () => item.style.backgroundColor = colorValue));
+  divs.forEach((item) => item.addEventListener('mousedown', () => item.style.backgroundColor = colorValue));
 });
+
+eraser.addEventListener('click', () => {
+  colorValue = null;
+})
+
+
 
