745a257e390ce59a4216fc2e4c0c97f1cd3d7d0f
David Forbes <davidforbesi@hotmail.com>
2023-06-21 09:56:27 +0100

    add prompt to set grid size

--- scripts.js
+++ scripts.js
@@ -1,5 +1,13 @@
 const gridContainer = document.querySelector('#grid-container');
-const divs = Array.from({ length: 256 }, () => document.createElement('div'));
+
+
+const rowSize = prompt('What size would you like your grid?');
+const gridTotal = Math.pow(rowSize, 2);
+
+gridContainer.style.cssText = `grid-template-columns: repeat(${rowSize}, 1fr); grid-template-rows: repeat(${rowSize}, 1fr);`;
+
+
+const divs = Array.from({ length: gridTotal }, () => document.createElement('div'));
 
 gridContainer.append(...divs);
 
