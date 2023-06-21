66421f68669c924ee1af35ab6c3b432835fef8ab
David Forbes <davidforbesi@hotmail.com>
2023-06-21 09:36:16 +0100

    added code so that div background changes after mouse hovers over it

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
 const gridContainer = document.querySelector('#grid-container');
-const divs = Array.from({ length: 256 }, (i) => document.createElement('div'));
+const divs = Array.from({ length: 256 }, () => document.createElement('div'));
+
 gridContainer.append(...divs);
+
+divs.forEach((item) => item.addEventListener('mouseover', () => item.style.backgroundColor = 'black'));
