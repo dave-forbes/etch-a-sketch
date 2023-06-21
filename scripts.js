1b25ef461b0b1638cabf1b22c0b5deb19f583db0
David Forbes <davidforbesi@hotmail.com>
2023-06-21 10:55:35 +0100

    adding button to prompt user to set size of grid

--- scripts.js
+++ scripts.js
@@ -1,14 +1,20 @@
 const gridContainer = document.querySelector('#grid-container');
+const button = document.querySelector('button');
 
+button.addEventListener('click', () => {
 
-const rowSize = prompt('What size would you like your grid?');
-const gridTotal = Math.pow(rowSize, 2);
+  gridContainer.innerHTML = '';
 
-gridContainer.style.cssText = `grid-template-columns: repeat(${rowSize}, 1fr); grid-template-rows: repeat(${rowSize}, 1fr);`;
+  const rowSize = prompt('What size would you like your grid?');
 
+  gridContainer.style.cssText = `grid-template-columns: repeat(${rowSize}, 1fr); grid-template-rows: repeat(${rowSize}, 1fr);`;
 
-const divs = Array.from({ length: gridTotal }, () => document.createElement('div'));
+  const gridTotal = Math.pow(rowSize, 2);
 
-gridContainer.append(...divs);
+  const divs = Array.from({ length: gridTotal }, () => document.createElement('div'));
 
-divs.forEach((item) => item.addEventListener('mouseover', () => item.style.backgroundColor = 'black'));
+  gridContainer.append(...divs);
+
+  divs.forEach((item) => item.addEventListener('mouseover', () => item.style.backgroundColor = 'black'));
+});
+
