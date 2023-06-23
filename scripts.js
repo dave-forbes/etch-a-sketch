4853557a425919676d06de689b23641594d2c63f
David Forbes <davidforbesi@hotmail.com>
2023-06-23 12:47:45 +0100

    changed theme of app to make it look more like an etch a sketch

--- scripts.js
+++ scripts.js
@@ -8,11 +8,25 @@ const divs = Array.from({ length: 2500 }, () => document.createElement('div'));
 gridContainer.style.cssText = `grid-template-columns: repeat(50, 1fr); grid-template-rows: repeat(50, 1fr);`;
 gridContainer.append(...divs);
 divs.forEach((item) => item.addEventListener('mouseover', () => item.style.backgroundColor = colorValue));
-
+colorButton.style.color = 'gold';
 
 color.addEventListener('change', () => colorValue = color.value);
+color.addEventListener('change', () => {
+  colorButton.style.color = 'gold';
+  eraser.style.color = 'black';
+})
 eraser.addEventListener('click', () => colorValue = null);
+eraser.addEventListener('click', () => {
+  eraser.style.color = 'gold';
+  colorButton.style.color = 'black';
+}
+);
 colorButton.addEventListener('click', () => colorValue = color.value);
+colorButton.addEventListener('click', () => {
+  colorButton.style.color = 'gold';
+  eraser.style.color = 'black';
+}
+);
 
 
 slider.addEventListener('change', () => {
